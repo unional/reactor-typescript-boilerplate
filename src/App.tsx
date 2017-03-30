1e4428e57a00185285d7d7bf21f917c2ed950239
Homa Wong <homawong@gmail.com>
2017-03-30 14:51:24 -0700

    Fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -14,75 +14,75 @@ Ext.require('Ext.plugin.Responsive');
 Ext.require('Ext.grid.plugin.ColumnResizing');
 
 export interface AppState {
-    employee: any
+  employee: any
 }
 
 /**
  * The main application view
  */
 export default class App extends Component<any, AppState> {
-    store: Ext.data.Store & any
-    constructor(props) {
-        super(props);
-        this.state = { employee: null };
-        this.store = {}
-        this.store = Ext.create('Ext.data.Store', {
-            fields: ['name', 'email', 'phone', 'hoursTaken', 'hoursRemaining'],
-            data
-        });
-    }
+  store: Ext.data.Store & any
+  constructor(props) {
+    super(props);
+    this.state = { employee: null };
+    this.store = {}
+    this.store = Ext.create('Ext.data.Store', {
+      fields: ['name', 'email', 'phone', 'hoursTaken', 'hoursRemaining'],
+      data
+    });
+  }
 
-    /**
-     * Filter the store when the user types in the search box
-     */
-    onSearch() {
-        let x: React.ReactInstance;
+  /**
+   * Filter the store when the user types in the search box
+   */
+  onSearch() {
+    let x: React.ReactInstance;
 
-        const query = (this.refs as any).query.getValue();
-        this.store.clearFilter();
+    const query = (this.refs as any).query.getValue();
+    this.store.clearFilter();
 
-        if (query.length) this.store.filterBy(record => {
-            const { name, email, phone } = record.data;
-            return name.indexOf(query) !== -1 || email.indexOf(query) !== -1 || phone.indexOf(query) !== -1;
-        });
-    }
+    if (query.length) this.store.filterBy(record => {
+      const { name, email, phone } = record.data;
+      return name.indexOf(query) !== -1 || email.indexOf(query) !== -1 || phone.indexOf(query) !== -1;
+    });
+  }
 
-    /**
-     * Update the component state based on the user's selection in the grid
-     */
-    onPersonSelect(grid, record) {
-        this.setState({ employee: record.data });
-    }
+  /**
+   * Update the component state based on the user's selection in the grid
+   */
+  onPersonSelect(grid, record) {
+    this.setState({ employee: record.data });
+  }
 
-    render() {
-        const { employee } = this.state;
+  render() {
+    const { employee } = this.state;
 
-        return (
-            <Container
-                plugins="responsive"
-                responsiveConfig={{
-                    tall: { layout: 'vbox' },
-                    wide: { layout: 'hbox' }
-                }}
-                >
-                <Panel title="Employees" layout="fit" flex={1} margin="20" shadow={true}>
-                    <Grid
-                        plugins={['columnresizing']}
-                        onSelect={this.onPersonSelect.bind(this)}
-                        store={this.store}
-                        columns={[
-                            { text: 'Name', dataIndex: 'name', flex: 2, resizable: true },
-                            { text: 'Email', dataIndex: 'email', flex: 3, resizable: true, plugins: 'responsive', responsiveConfig: { tall: { hidden: true } } },
-                            { text: 'Phone', dataIndex: 'phone', flex: 2, resizable: true }
-                        ]}
-                        >
-                        <Container layout="hbox" docked="top" padding={5}>
-                            <SearchField ref="query" placeHolder="Search..." flex={1} onChange={this.onSearch.bind(this)} />
-                        </Container>
-                    </Grid>
-                </Panel>
-                {employee && <Employee employee={employee} onCloseClick={() => this.setState({ employee: null })} />}
+    return (
+      <Container
+        plugins="responsive"
+        responsiveConfig={{
+          tall: { layout: 'vbox' },
+          wide: { layout: 'hbox' }
+        }}
+        >
+        <Panel title="Employees" layout="fit" flex={1} margin="20" shadow={true}>
+          <Grid
+            plugins={['columnresizing']}
+            onSelect={this.onPersonSelect.bind(this)}
+            store={this.store}
+            columns={[
+              { text: 'Name', dataIndex: 'name', flex: 2, resizable: true },
+              { text: 'Email', dataIndex: 'email', flex: 3, resizable: true, plugins: 'responsive', responsiveConfig: { tall: { hidden: true } } },
+              { text: 'Phone', dataIndex: 'phone', flex: 2, resizable: true }
+            ]}
+            >
+            <Container layout="hbox" docked="top" padding={5}>
+              <SearchField ref="query" placeHolder="Search..." flex={1} onChange={this.onSearch.bind(this)} />
             </Container>
-        )
-    }
+          </Grid>
+        </Panel>
+        {employee && <Employee employee={employee} onCloseClick={() => this.setState({ employee: null })} />}
+      </Container>
+    )
+  }
 }
